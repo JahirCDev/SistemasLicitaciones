defbc701f76fdf57b303d8a69a681d1dad870c5c
Jahir1515 <166346937+Jahir1515@users.noreply.github.com>
2026-09-03 04:07:35 -0500

    feat: add centralized imports from services

---
+++ frontend/src/services/index.js
@@ -0,0 +1,5 @@
+export * as clientesService from "./clientesService";
+export * as productosService from "./productosService";
+export * as licitacionesService from "./licitacionesService";
+export * as usersService from "./usersService";
+
